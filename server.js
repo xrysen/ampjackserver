60a02473bde5fa72134c99b8c3d4f057a6fcddb5
Sean Oyler <xrysen@gmail.com>
2022-02-08 15:59:46 -0800

    Adding logs

--- server.js
+++ server.js
@@ -130,6 +130,7 @@ const getDealerTotal = (dealersHand) => {
 }
 
 app.use((req, res, next) => {
+    console.log(req.session.id)
     if(!req.session.initialised) {
         req.session.initialised = true;
         req.session.playerTotal = 0;
