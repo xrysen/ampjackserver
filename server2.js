2e871917569ac600c093d952c2cd48432b0b3d2b
Sean Oyler <xrysen@gmail.com>
2022-02-09 16:26:36 -0800

    Updates session variable

--- server2.js
+++ server2.js
@@ -11,10 +11,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: {
-    sameSite: 'none',
-    httpOnly: true,
-    secure: true,
-    maxAge: 1000 * 60 * 60 * 24 * 30
+    maxAge: 3600000,secure: false, httpOnly: true
 },
 }))
 
